da83d767333d3278297c4ca10e7f62bbcea3d47c
Addiction2251 <sanskarsingh23092002@gmail.com>
2022-04-17 22:35:23 +0530

    Creating JavaScript file to handle the index page

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+var createPostModal = document.getElementById("createPostModal");
+
+// Search for create post button
+var createPostBtn = document.getElementById("createPostBtn");
+
+// Search close button
+var closeCreatePost = document.getElementById("closeCreatePost");
